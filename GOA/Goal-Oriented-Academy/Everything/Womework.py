38f9ef48f484a5592ef300b9067a9d66cd5ae1e8
miro772255 <147084856+miro772255@users.noreply.github.com>
2023-11-28 18:57:48 +0400

    Add files via upload

---
+++ GOA/Goal-Oriented-Academy/Everything/Womework.py
@@ -0,0 +1,54 @@
+# A code with 28 lines and 14 errors
+
+#print ("Miriani mskhiladze")
+#print ("Mirian mskhiladze aris "    +   " 13 "   +   " wlis ") # + ბრჭყალებში არ უნდა ჩასვა.
+
+#name = ("Mirian")
+#surname = ("Mskhiladze")
+#age = ("13") #name და surname სტრინგის ტიპის ცვლადებია, ხოლო age ინტერეჯერი, ამიტომ str+int is error.
+#height = "168.2"
+#knows_programming = True # True და False ყოველთვის იწყება დიდი ასოთი.
+#knows_playing_tennis = False
+#print (name + " " + surname + " " + age + " " + height)
+#personal_information = ("My name is Miro,  I am 13 years old,  playing basketball and studying in GOAL ORIENTED ACADEMY")
+#print (personal_information)
+
+# Now I am starting to make 14 errors.
+#print (Miriani Mskhilaadze)
+#print ("Miriani Mskhiladze aris 13 wlis 
+
+
+# P.S. error-ები არ გააკეთა აშკარად არასწორ კოდებზე და არვიცი, 3 შეცდომა დაწერა მარა დანარჩენებზე რაც სწორად ეწერა და შეცდომა მინდოდა რო გამეკეთებინა არ შეცვალა ისევ 3 error-ს წერდა.
+
+
+
+
+
+
+
+
+
+#a = 9
+#name = (  "Bob"  )
+#print ( name * a )
+
+# a = 7
+# user_name = input("Enter your name please") 
+
+# name_length = len(user_name)
+
+# print (name_length * user_name)
+
+# enter your name-ში თუ space-ის გარეშე დაწერ სახელს, დაპრინტავს იმდენჯერ რამდენი ასოც არის რელურად სახელში, მაგრამ თუ დაწერ space-ით იმ space-საც სტრინგ სიმბოლოდ აღიქვამს და ერთით მეტს ბეჭდავს!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
